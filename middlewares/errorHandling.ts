beef8510c63898031c09f6d296d9f31a18a203df
Piotr Borończyk <boronczyk.piotr@gmail.com>
2023-02-08 22:20:02 +0100

    add error handler to app

--- middlewares/errorHandling.ts
+++ middlewares/errorHandling.ts
@@ -1,5 +1,12 @@
 import { Request, Response, NextFunction } from 'express'
 import httpStatus from 'http-status'
+import { ApiError } from '../utils/ApiError'
+
+// eslint-disable-next-line no-unused-vars
+export const errorHandler = (err: ApiError, _req: Request, res: Response, _next: NextFunction) => {
+    const {message, statusCode} = err
+    res.status(statusCode).send(message)
+}
 
 export const errorConverter = (err: Error, _req: Request, _res: Response, next: NextFunction) => {
     if (!(err instanceof ApiError)) {
