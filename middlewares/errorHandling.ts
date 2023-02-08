246f1f7a456d34fab3736f1de52c78b077451b23
Piotr Borończyk <boronczyk.piotr@gmail.com>
2023-02-08 22:32:36 +0100

    handle exceptions and send meta as a response

--- middlewares/errorHandling.ts
+++ middlewares/errorHandling.ts
@@ -5,12 +5,20 @@ import { ApiError } from '../utils/ApiError'
 // eslint-disable-next-line no-unused-vars
 export const errorHandler = (err: ApiError, _req: Request, res: Response, _next: NextFunction) => {
     const {message, statusCode} = err
-    res.status(statusCode).send(message)
+    res.status(statusCode).json({
+        success: false,
+        meta: {
+            code: statusCode,
+            errorText: httpStatus[statusCode],
+            message: message
+        },
+        data: {}
+    })
 }
 
 export const errorConverter = (err: Error, _req: Request, _res: Response, next: NextFunction) => {
     if (!(err instanceof ApiError)) {
-        next(new ApiError(httpStatus.BAD_REQUEST, err.message))
+        next(new ApiError(httpStatus.INTERNAL_SERVER_ERROR, err.message))
     }
     next(err) 
 }
